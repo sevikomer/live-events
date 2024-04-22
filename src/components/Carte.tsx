eec0bd3c5268b4555a4e53199e9a15f482f908d7
Omer Sevik <omer.sevik@hotmail.fr>
2024-04-22 21:37:57 +0200

    Update Carte.tsx

--- src/components/Carte.tsx
+++ src/components/Carte.tsx
@@ -61,7 +61,7 @@ function getFilteredList() {
                 <h1 className='lg:text-6xl text-4xl font-extrabold text-center pt-2'>CARTE INTERACTIVE</h1>
             </div>
             <div className=''>
-           <Filter onSelect={setSelectedCategory}
+           <Filter setSelectedCategory={updateSelectedCategories}
            selectedCategory={selectedCategory}
            resetSelectedCategories={resetSelectedCategories}/>
                     <div>
